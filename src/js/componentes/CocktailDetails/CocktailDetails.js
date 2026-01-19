efefb832fd5b1d1e1113d4cd64e7c781fa7d29b5
Mirco Dascoli <mircodascoli3@gmail.com>
2026-01-19 16:09:24 +0100

    Add CocktailDetails component and update CocktailList to use it

--- src/js/componentes/CocktailDetails/CocktailDetails.js
+++ src/js/componentes/CocktailDetails/CocktailDetails.js
@@ -1,20 +1,15 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
+
 export class CocktailDetails extends LitElement {
   static properties = {
     cocktail: { type: Object }
   };
 
   render() {
-    if (!this.cocktail) return html``;
-
-    const { name, image } = this.cocktail;
-
     return html`
       <li>
-        <h3>${name}</h3>
-        <p>${image}</p>
-
-        </ul>
+        <h2>${this.cocktail.name}</h2>
+        <img src="${this.cocktail.image}" alt="${this.cocktail.name}" />
       </li>
     `;
   }
