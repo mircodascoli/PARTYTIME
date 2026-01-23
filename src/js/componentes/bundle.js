f90b9cc0e4426162679419532f01a27ddf52ac6c
Mirco Dascoli <mircodascoli3@gmail.com>
2026-01-23 19:31:42 +0100

    Implement DescriptionPopUp component and integrate it with CocktailList and main.js for event handling

--- src/js/componentes/bundle.js
+++ src/js/componentes/bundle.js
@@ -4,4 +4,5 @@ import { SignInFormLit } from "./SignInFormLit/SignInFormLit.js"
 import {Calculador } from "./calculador/calculador.js"
 import { MyParty } from "./myParty/MyParty.js";
 import { CocktailList } from "./CocktailList/CocktailList.js";
-export { SignInFormLit, LoginInFormLit, Calculador, MyParty, CocktailList }; 
+import { DescriptionPopUp } from "./DescriptionPopUp/DescriptionPopUp.js";
+export { SignInFormLit, LoginInFormLit, Calculador, MyParty, CocktailList, DescriptionPopUp }; 
