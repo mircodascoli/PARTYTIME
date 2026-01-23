4c511eabc8a28b139421b13d51534d529e457c77
Mirco Dascoli <mircodascoli3@gmail.com>
2026-01-23 13:25:54 +0100

    The cocktail list is back in the UI

--- src/js/componentes/CocktailDetails/CocktailDetails.js
+++ src/js/componentes/CocktailDetails/CocktailDetails.js
@@ -1,15 +1,17 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
-
+import CocktailDetailsCSS from '../CocktailDetails/CocktailDetailsCSS.css' with { type: 'css' };
+import ResetCSS from '../../../css/reset.css' with { type: 'css' };
 export class CocktailDetails extends LitElement {
+    static styles = [ResetCSS, CocktailDetailsCSS];
   static properties = {
     cocktail: { type: Object }
   };
 
   render() {
     return html`
-      <li>
+      <li class="cocktail-item">
         <h2>${this.cocktail.name}</h2>
-        <img src="${this.cocktail.image}" alt="${this.cocktail.name}" />
+        <img src="../../img/imgCocktails/${this.cocktail.name}.jpeg" alt="${this.cocktail.name}" class="img-list-cocktails" />
       </li>
     `;
   }
