5fb454a55fb20ab4e33328945dbee34867e78895
Mirco Dascoli <mircodascoli3@gmail.com>
2026-02-01 11:41:29 +0100

    Creating new Calculator component in calculadores

--- src/js/componentes/bundle.js
+++ src/js/componentes/bundle.js
@@ -1,8 +1,9 @@
 
 import {LoginInFormLit} from "./LoginFormLit/loginFormLit.js"
 import { SignInFormLit } from "./SignInFormLit/SignInFormLit.js"
-import {Calculador } from "./calculador/calculador.js"
+import { Calculator } from "./Calculator/Calculator.js"
 import { MyParty } from "./myParty/MyParty.js";
 import { CocktailList } from "./CocktailList/CocktailList.js";
 import { DescriptionPopUp } from "./DescriptionPopUp/DescriptionPopUp.js";
-export { SignInFormLit, LoginInFormLit, Calculador, MyParty, CocktailList, DescriptionPopUp }; 
+
+export { SignInFormLit, LoginInFormLit, Calculator, MyParty, CocktailList, DescriptionPopUp }; 
