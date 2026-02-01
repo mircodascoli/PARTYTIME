8671f37b10fa20f99ad30fc3a9e689e0934d556c
Mirco Dascoli <mircodascoli3@gmail.com>
2026-02-01 11:48:45 +0100

    Calculator component now displays

--- src/js/componentes/bundle.js
+++ src/js/componentes/bundle.js
@@ -1,7 +1,7 @@
 
-import {LoginInFormLit} from "./LoginFormLit/loginFormLit.js"
-import { SignInFormLit } from "./SignInFormLit/SignInFormLit.js"
-import { Calculator } from "./Calculator/Calculator.js"
+import {LoginInFormLit} from "./LoginFormLit/loginFormLit.js";
+import { SignInFormLit } from "./SignInFormLit/SignInFormLit.js";
+import { Calculator } from "./Calculator/Calculator.js";
 import { MyParty } from "./myParty/MyParty.js";
 import { CocktailList } from "./CocktailList/CocktailList.js";
 import { DescriptionPopUp } from "./DescriptionPopUp/DescriptionPopUp.js";
