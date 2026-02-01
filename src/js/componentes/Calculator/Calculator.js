f60aa42f55fb1d327f377d304cbe12ed2a736ac9
Mirco Dascoli <mircodascoli3@gmail.com>
2026-02-01 11:41:29 +0100

    Creating new Calculator component in calculadores

---
+++ src/js/componentes/Calculator/Calculator.js
@@ -0,0 +1,19 @@
+import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
+import CalculatorCSS from '../Calculator/CalculatorCSS.css' with { type: 'css' };
+import ResetCSS from '../../../css/reset.css' with { type: 'css' };
+export class Calculator extends LitElement {
+ static styles = [ResetCSS, CalculatorCSS];
+  constructor() {
+    super();
+  }
+
+  render() {
+   return html`
+          <div class="Calculador-Container">
+            <h2>Calculador Component</h2>
+          </div>
+    `;
+  }
+}
+
+customElements.define('calculator', Calculator);
