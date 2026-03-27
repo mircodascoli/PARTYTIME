2c87462fad2b0a4322128d49161eb1747d4b74b1
Mirco Dascoli <mircodascoli3@gmail.com>
2026-03-27 15:58:32 +0100

    Created and imported new " TotalCart.js" to carrito html page to show cart total to user

--- src/js/componentes/bundle.js
+++ src/js/componentes/bundle.js
@@ -7,6 +7,7 @@ import { CocktailList } from "./CocktailList/CocktailList.js";
 import { DescriptionPopUp } from "./DescriptionPopUp/DescriptionPopUp.js";
 import { PreCartPopUp } from "./PreCartPopUp/PreCartPopUp.js";
 import { CartList } from "./CartList/CartList.js";
-import {DropDownCartMenu} from "./DropDownCartMenu/DropDownCartMenu.js"; 
+import { DropDownCartMenu }  from "./DropDownCartMenu/DropDownCartMenu.js"; 
+import { TotalCart } from "./TotalCart/TotalCart.js";
 
-export { SignInFormLit, LoginInFormLit, Calculator, MyRecipes, CocktailList, DescriptionPopUp, PreCartPopUp, CartList,  DropDownCartMenu  }; 
+export { SignInFormLit, LoginInFormLit, Calculator, MyRecipes, CocktailList, DescriptionPopUp, PreCartPopUp, CartList,  DropDownCartMenu, TotalCart }; 
