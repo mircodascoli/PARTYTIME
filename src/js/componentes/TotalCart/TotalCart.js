9230f8d18f78548f9f3f2963bd513a442f860a95
Mirco Dascoli <mircodascoli3@gmail.com>
2026-04-06 12:19:43 +0200

    removed console logs and preparing envirioment for the next task

--- src/js/componentes/TotalCart/TotalCart.js
+++ src/js/componentes/TotalCart/TotalCart.js
@@ -2,40 +2,27 @@ import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit
 import ResetCSS from '../../../css/reset.css' with { type: 'css' };
 import TotalCartCSS from '../TotalCart/TotalCartCSS.css' with { type: 'css' };
 
-
 export class TotalCart extends LitElement {
   static styles = [ResetCSS, TotalCartCSS];
+
   static properties = {
-    data : { type: Array},
+    total: { type: Number },
   };
- constructor() {
+
+  constructor() {
     super();
-     this.data = [];
+    this.total = 0;
   }
- connectedCallback() {
-    super.connectedCallback();
-    this.getTotalCart();
-    
+
+  render() {
+    console.log('TotalCart updated, total:', this.total)
+    return html`
+      <div class="total-cart">
+        <h2>Total:</h2>
+        <p>${this.total} €</p>
+      </div>
+    `;
   }
-    getTotalCart() {
-      console.log('get total cart', this.data)
- return this.total = parseFloat(
-    this.data.reduce((sum, item) => sum + (item.price * item.quantity), 0).toFixed(2)
-  );
-  
-     
-  };
- 
-
-
- render() {
-  
-  return html`
-    <div class="total-cart"></div>
-      <h2>Total:</h2>
-        <p>${this.total}</p>
-    </div>      
-  `;
- }
 }
+
 customElements.define('total-cart', TotalCart);
