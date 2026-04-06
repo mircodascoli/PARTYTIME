1e6b05d4fec2d204a5a59a94f438cf3d9fbdffd8
Mirco Dascoli <mircodascoli3@gmail.com>
2026-04-06 17:22:53 +0200

    The productList component fetches and displays each product correctly

--- src/js/componentes/ProductsList/ProductsList.js
+++ src/js/componentes/ProductsList/ProductsList.js
@@ -1,7 +1,7 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
 import ResetCSS from '../../../css/reset.css' with { type: 'css' };
 import ProductsListCSS from '../ProductsList/ProductsListCSS.css' with { type: 'css' };
-/* import { getAPIData, API_PORT } from '../../main.js'; */
+import { getAPIData, API_PORT } from '../../main.js'; 
 
 export class ProductsList extends LitElement {
   static styles = [ResetCSS, ProductsListCSS];
@@ -17,27 +17,34 @@ export class ProductsList extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-   /*  this.getBottles(); */
+    this.getBottles();
   }
 
- /*  async getBottles() {
+   async getBottles() {
     try {
-     let data = await getAPIData(
-        `${location.protocol}//${location.hostname}${API_PORT}/api/read/cocktails`,
-        'GET'
-      );
+     let data = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/api/read/botellas`, 'GET')
 
-      this.cocktails = data;
+      this.bottles = data;
 
-      console.log(this.cocktails, 'cocktails mapped to model'); 
+      console.log(this.bottles, 'bottles mapped to model'); 
     } catch ( error ) {
-   console.error('Error fetching cocktails:', error); 
+   console.error('Error fetching bottles:', error); 
     }
-  } */
+  } 
 
  render() {
   return html`
-<p>Productos</p>
+    <ul class="products-list">
+      ${this.bottles.map(bottle => html`
+        <li class="product-item">
+          <img src="../../img/imgProductos/${bottle.name}.png" alt="${bottle.name}" class="product-image" />
+          <h2>${bottle.name}</h2>
+          <p>${bottle.price} €</p>
+          <p>${bottle.description}</p>
+          <button class="add-to-cart-button" @click=${() => this.addToCart(bottle)}>Add to Cart</button> <!-- this will open the pre-cart with the selected product and quantity 1 by default, then the user can change the quantity with the dropdown menu in the pre-cart
+        </li> -->
+      `)}
+    </ul>
   `; 
  }
 
