2696fea9dfa95aebce78d0596355b64c9b6c0c05
Mirco Dascoli <mircodascoli3@gmail.com>
2026-04-09 11:59:06 +0200

    Refactor imports to use main.js in MyRecipes and Shop components; add getAPIData function in utils.js

--- src/js/main.js
+++ src/js/main.js
@@ -160,3 +160,13 @@ function welcoming(){
   ${userName}`
 }
 
+export function launchpreCartPoPup(ing) {
+    console.log('buy ingredient function activated by the click', ing);
+
+    window.dispatchEvent(new CustomEvent('ingredient-selected', {
+      detail: ing,
+      bubbles: true,
+      composed: true
+    }));
+  
+  } 
