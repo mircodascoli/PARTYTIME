ea5b97a12741dcb32fad548994777092287eb3a3
Mirco Dascoli <mircodascoli3@gmail.com>
2026-04-16 12:16:51 +0200

    Integrate Supabase for authentication and enhance LoginForm with Google login option

---
+++ src/config/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://idgwdqpndasbhmsmzqll.supabase.co',
+  'sb_publishable_qt-qCP9cXV1KFKw38UjXaQ_gz1eKg_T'
+);
