bb46dbb05e2b2bb5a47c0e0190c6667c0740c6a2
Mirco Dascoli <mircodascoli3@gmail.com>
2026-05-18 12:10:57 +0200

    Refactor CartList and TotalCart components; enhance markup structure, improve styling, and format price display

--- src/js/componentes/TotalCart/TotalCart.js
+++ src/js/componentes/TotalCart/TotalCart.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
 import ResetCSS from '../../../css/reset.css' with { type: 'css' };
 import TotalCartCSS from '../TotalCart/TotalCartCSS.css' with { type: 'css' };
+import { formatPrice } from '../../utils.js';
 
 export class TotalCart extends LitElement {
   static styles = [ResetCSS, TotalCartCSS];
@@ -17,10 +18,19 @@ export class TotalCart extends LitElement {
   render() {
     console.log('TotalCart updated, total:', this.total)
     return html`
+
+      <h2 class="order-summary">Order Summary</h2>
+     <div class="items-total">
+      <p>Items Total</p> <p>${formatPrice(this.total)}</p>
+     </div> 
+     <div class="delivery">
+      <p class="delivery-title">Shipping:</p> <p class="delivery-free">Free</p>
+     </div>
       <div class="total-cart">
-        <h2>Total:</h2>
-        <p>${this.total} €</p>
+      <h2>Total:</h2> <p>${formatPrice(this.total)}</p>
       </div>
+      <button class="checkout-button">Checkout</button>
+      <p class="disclaimer">This app is for practical purposes only. No real orders will be processed.</p>
     `;
   }
 }
