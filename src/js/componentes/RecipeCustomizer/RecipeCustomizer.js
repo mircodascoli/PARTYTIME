24b15c46d811f480b09f242c44fcc86f640837f0
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-08 18:13:23 +0200

    calculator component moves inside Recipe costumizer

--- src/js/componentes/RecipeCustomizer/RecipeCustomizer.js
+++ src/js/componentes/RecipeCustomizer/RecipeCustomizer.js
@@ -1,15 +1,21 @@
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
 import RecipeCustomizerCSS from '../RecipeCustomizer/RecipeCustomizerCSS.css' with { type: 'css' };
 import ResetCSS from '../../../css/reset.css' with { type: 'css' };
+
 export class RecipeCustomizer extends LitElement {
- static styles = [ResetCSS, RecipeCustomizerCSS];
- static properties = { cocktail: { type: Object }};
+  static styles = [ResetCSS, RecipeCustomizerCSS];
+  static properties = {
+    cocktail: { type: Object },
+    activeView: { type: String },
+    selectedRecipe: { type: Object },
+  };
+
   constructor() {
     super();
     this.cocktail = null;
+    this.activeView = 'A';
+    this.selectedRecipe = null;
   }
-
- 
 
   render() {
     if (!this.cocktail) {
@@ -19,27 +25,25 @@ export class RecipeCustomizer extends LitElement {
         </div>
       `;
     }
-   return html`
-          
-    <div class="popup-window">
-      <div class="popup-close-button container">
-        <button class="popup-close-button" @click="${() => this.remove()}">X</button>
+    return html`
+      <div class="popup-window" @switch-view="${this._onSwitchView}">
+        <div class="popup-close-button-container">
+          <button class="popup-close-button" @click="${() => this.remove()}">X</button>
+        </div>
+        ${this.activeView === 'A'
+          ? html`<cocktail-info .cocktail="${this.cocktail}"></cocktail-info>`
+          : html`<calculator-component .recipe="${this.selectedRecipe}"></calculator-component>`}
       </div>
-     <cocktail-info .cocktail="${this.cocktail}"></cocktail-info>
-     </div>
-
     `;
   }
-   SelectRecipeToCalc(recipeSelected) {
-    try {
-  sessionStorage.setItem('selectedCraft', JSON.stringify(recipeSelected))
-  }catch (err) {
-    console.error('Error setting cocktail to session storage', err);
-    alert('Something went wrong when selecting the cocktail');
-  }
-    finally {
-    location.href = './calculator.html'
+
+  _onSwitchView(e) {
+    const { next, recipeSelected } = e.detail;
+    this.activeView = next;
+    if (recipeSelected) {
+      this.selectedRecipe = recipeSelected;
     }
+  }
 }
-}
+
 customElements.define('recipe-customizer', RecipeCustomizer);
