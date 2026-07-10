fb59226efdf93184a7d4699d3de19651cdb5c1b8
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-10 16:54:03 +0200

    Refactor MyRecipes component layout and styles for improved user experience

--- src/js/componentes/MyRecipes/MyRecipes.js
+++ src/js/componentes/MyRecipes/MyRecipes.js
@@ -76,54 +76,69 @@ export class MyRecipes extends LitElement {
     }
   } 
 
-
-  render() {
+render() {
   const recipes = this.apiData?.recipes ?? [];
-  
+
   return html`
-<div class="my-recipes-container">
-  <p>My recipes</p>
-  <ul class="recipes-list">
-    ${recipes.length === 0
-      ? html`<p>No recipes saved.</p>`
-      : recipes.map(item => html`
-        <li class="recipe-card">
-          <div class="recipe-header">
-            <img src="../../../img/imgCocktails/${item.name}.png" alt="${item.name}" class="recipe-preview-image" />
-            <p>${item.name} × ${item.amount}</p>
-            <p class="recipe-preview-description">${item.description}</p>
-            <button class="delete-recipe" @click=${() => this.deleteRecipe(item._id)}>x</button>
-          </div>
+    <div class="my-recipes-container">
+      <ul class="recipes-list">
+        ${recipes.length === 0
+          ? html`<p>No recipes saved yet.</p>`
+          : html`
+              <p class="your-recipes-p">Your recipes:</p>
+              ${recipes.map(item => html`
+                <li class="recipe-card">
+                  <div class="delete-recipe-container">
+                    <button class="delete-recipe" @click=${() => this.deleteRecipe(item._id)}>x</button>
+                  </div>
 
-          <div class="recipe-data">
-            <div class="recipe-ingredients-container">              
+                  <div class="recipe-header">
+                    <img src="../../../img/imgCocktails/${item.name}.png" alt="${item.name}" class="recipe-preview-image" />
+                    <div class="name-desciption-container">
+                      <p><span class="span-recipe-title">${item.name} </span> × ${item.amount}</p>
+                      <p class="recipe-preview-description">${item.description}</p>
+                    </div>
+                  </div>
 
-              <ul class="recipe-ingredients-list">
-                ${item.ingredientes.map(ing => html` <li class="recipe-ingredient">${ing.name} ${ing.mls}ml</li>`)}
-              </ul>
-            </div>
+                  <div class="recipe-data">
+                    <div class="recipe-ingredients-container">
+                      <ul class="recipe-ingredients-list">
+                        <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clipboard-data"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 5a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2" /><path d="M9 17v-4" /><path d="M12 17v-1" /><path d="M15 17v-2" /><path d="M12 17v-1" /></svg> <span class="span-grey">ingredients:</span></p>
+                        ${item.ingredientes.map(ing => html`
+                          <li class="recipe-ingredient"><span> &#x2022;</span> ${ing.name} ${ing.mls} ml</li>
+                        `)}
+                      </ul>
+                      <hr class="section-divider" />
+                    </div>
 
-            <div class="serving-container">
-              <p class="serving-description">${item.serving}</p>
-            </div>
+                    
 
-            <div class="recipe-ingredients-container">
-              <ul class="recipe-products-list">
-                ${item.ingredientes.map(ing => html`
-                  <li class="product-item">
-                    <img src="../../img/imgProductos/${ing.dbname}.png" alt="${ing.dbname}" class="suggested-product-image" @click=${() => launchpreCartPoPup(ing.dbname)} @error=${(e) => e.target.src = '../../img/fallback.png'} />
-                    <p>${ing.dbname}</p>
-                    <button class="buy-button" @click=${() => launchpreCartPoPup(ing.dbname)}>BUY</button>
-                  </li>
-                `)}
-              </ul>
-            </div>
-          </div>
-        </li>
-      `)
-    }
-  </ul>
-</div>
+                    <div class="serving-container">
+                      <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-milkshake"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M17 10a5 5 0 0 0 -10 0" /><path d="M6 11a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1l0 -1" /><path d="M7 13l1.81 7.243a1 1 0 0 0 .97 .757h4.44a1 1 0 0 0 .97 -.757l1.81 -7.243" /><path d="M12 5v-2" /></svg><span class="span-grey"> Serving suggestion:</span></p>
+                      <p class="serving-description">${item.serving}</p>
+                       <hr class="section-divider" />
+                    </div>
+
+                   
+
+                    <div class="recipe-ingredients-container">
+                      <ul class="recipe-products-list">
+                        ${item.ingredientes.map(ing => html`
+                          <li class="product-item">
+                            <img src="../../img/imgProductos/${ing.dbname}.png" alt="${ing.dbname}" class="suggested-product-image" @click=${() => launchpreCartPoPup(ing.dbname)} @error=${(e) => e.target.src = '../../img/fallback.png'} />
+                            <p class= "p-ingredient-name">${ing.dbname}</p>
+                            <button class="btn-black" @click=${() => launchpreCartPoPup(ing.dbname)}>BUY</button>
+                          </li>
+                        `)}
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              `)}
+            `
+        }
+      </ul>
+    </div>
   `;
 }
 
