ea182937b49b6fda4e29d24c176a0f2a4ea9dcce
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-11 15:02:20 +0200

    Enhance layout and styles for LogOut and Welcoming components; add icons to buttons

--- src/js/componentes/LogOut/LogOut.js
+++ src/js/componentes/LogOut/LogOut.js
@@ -14,7 +14,7 @@ export class LogOut extends LitElement {
 
   render() {
     return html`
-      <button class="log-out-button" @click="${this._LogOut}">Log Out</button>
+      <button class="log-out-button" @click="${this._LogOut}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-power"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M7 6a7.75 7.75 0 1 0 10 0" /><path d="M12 4l0 8" /></svg></button>
     `;
   }
 }
