28562bde65b1e43a9f472887aabac07c2da3f06d
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-11 15:02:20 +0200

    Enhance layout and styles for LogOut and Welcoming components; add icons to buttons

--- src/js/componentes/Welcoming/Welcoming.js
+++ src/js/componentes/Welcoming/Welcoming.js
@@ -33,12 +33,15 @@ connectedCallback() {
     return html`
         <div class="welcome-container">
           <div class="hello-container">
-            <h1 id="welcome-text">Welcome, ${this.name}</h1>
-            <p>Here you can find your favorite recipes</p>
-            <logout-component></logout-component>
+            <div class="user-log-container">
+             <div id="welcome-text"><span class="welcome-text"><span class="welcome-span">Welcome,</span> <span class="user-name-span">${this.name}</span></div>
+             <logout-component></logout-component>
+            </div>
+            <p>Here you can find your favorite recipes</p> 
           </div>
+
           <div class="button-new-recipe-container">
-               <button class="add-button" @click=${this.addRecipe}> Add New Recipe</button>
+              <button class="add-button" @click=${this.addRecipe}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg> Add New Recipe</button>
             </div>
         </div>
     `;
