90834365f93f6064ea418262fbde27958477cac1
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-20 19:38:56 +0200

    Enhance layout and styling for CocktailInfo and PreCartPopUp components; improve responsiveness and user interaction

--- src/js/componentes/CocktailInfo/CocktailInfo.js
+++ src/js/componentes/CocktailInfo/CocktailInfo.js
@@ -23,19 +23,23 @@ export class CocktailInfo extends LitElement {
     
     return html`
     <div class="cocktail-info-data-container">
+
       <div class="cocktail-info-pic-container"> 
         <img src="../../img/imgCocktails/${this.cocktail.name}.png" alt="${this.cocktail.name}" class="img-popup-cocktails" />
       </div>
+      
       <div class="cocktail-info-title-description-container">
         <h2 class="cocktail-info-title">${this.cocktail.name}</h2>
         <p class="popup-description">${this.cocktail.description}</p>
       </div>  
+
       <div class="cocktail-info-button-container">
         <button class="popup-select-button" @click="${() => this.SelectRecipeToCalc(this.cocktail) }">
           <span>NEXT</span>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>  
         </button>
       </div>
+
     </div>  
     `;
   }
