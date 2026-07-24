70540d26e4c786e1de23afc2c4dce2572e317deb
Mirco Dascoli <mircodascoli3@gmail.com>
2026-07-24 22:41:07 +0200

    Refactor Calculator component layout and styles; enhance user instructions and button functionality for improved interaction

--- src/js/componentes/Calculator/Calculator.js
+++ src/js/componentes/Calculator/Calculator.js
@@ -43,14 +43,19 @@ export class Calculator extends LitElement {
      return html`
      
       <div class="container-calculator">
-        <div>
+        <div class="recipe-name-container">
            <h2 class="calculator-recipe-name">${this.recipe.name} </h2>
+        </div>
+
+        <div class="instructions-container">
+           <p class="instructions">Move the slider to increase or decrease your batch quantity</p>
         </div>
          
         <div class="input-calculator">
           <label for="range" id="label-range">Quantity:</label>
           <input id="range" type="range" min="1" max="10" step="1" value="5" @input=${this.handleRange}/>
-          <p class="people-units"> for: ${this.quantity}[i]</p>
+          <p class="people-units"> for: ${this.quantity}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 7a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
         </div>
 
         <div id="resultados">
@@ -69,10 +74,16 @@ export class Calculator extends LitElement {
         </div>  
 
          <div class="calculator-button-container">       
-          <button class="save-recipe-button" @click="${this.handleSaveClick}">[i] Save and party!</button>
-          <button class="another-recipe-button" @click="${this.handleOthersClick}">[i] Another recipe</button>
+          <button class="save-recipe-button" @click="${this.handleSaveClick}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-clipboard-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 5a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2" /><path d="M10 14h4" /><path d="M12 12v4" /></svg>
+            <span>Save and party!</span>
+          </button>
+          <button class="another-recipe-button" @click="${this.handleOthersClick}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-back-up"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 14l-4 -4l4 -4" /><path d="M5 10h11a4 4 0 1 1 0 8h-1" /></svg>  
+            <span>Another recipe</span>
+          </button>
         </div>
-        
+
       </div>
     `;
   }
