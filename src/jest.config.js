d9a006165014aa29caa3898a96cb87594f70d589
Mirco Dascoli <mircodascoli3@gmail.com>
2026-08-28 18:37:14 +0200

    uninstalled dependencies that caused conflict with the project.

---
+++ src/jest.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^clases/(.*)': '<rootDir>/src/js/clases/*.js',
+    '^libreria/(.*)': '<rootDir>/src/js/libreria/*.js',
+  },
+};
+
+export default config;
